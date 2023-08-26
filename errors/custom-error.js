0c5bca51056655286c7d6eb12ba5709e98df4ebe
deeksha45rai <deeksharai6559@gmail.com>
2023-08-26 16:08:25 +0530

    added files

---
+++ errors/custom-error.js
@@ -0,0 +1,12 @@
+class CustomAPTError extends Error{
+    constructor(message,statusCode){
+        super(message)
+        this.statusCode = statusCode
+    }
+}
+
+const createCustomError = (msg, statusCode)=>{
+    return new CustomAPIError(msg,statusCode)
+}
+
+module.exports={ createCustomError, CustomAPTError}
